07c241f3cbdec68a038b728e9840867fa098884a
Pikevchu <gallien@et.esiea.fr>
2020-05-30 04:59:37 +0200

    file explorer and opener

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Small clock script.
+/// </summary>
+[RequireComponent(typeof(Text))]
+public class Clock : MonoBehaviour
+{
+    /// <summary>
+    /// Clock display text.
+    /// </summary>
+    [SerializeField]
+    private Text m_ClockDisplay = null;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        m_ClockDisplay.text = System.DateTime.Now.ToString("HH : mm");
+    }
+}
