b580c33135c1bf8be4aa674313ba89849f9d2ea1
Pikevchu <gallien@et.esiea.fr>
2020-05-30 18:08:35 +0200

    Merge branch 'master' into LevelManagement

---
+++ Assets/Scripts/HealthBarManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class HealthBarManager : MonoBehaviour
+{
+    private Slider      m_slider = null;
+
+    [SerializeField]
+    private Gradient    m_gradient;
+    [SerializeField]
+    private Image       m_fill;
+
+    private void Start()
+    {
+        m_slider = GetComponent<Slider>();
+    }
+
+    public void SetMaxHealth(int p_health)
+    {
+        if (m_slider == null)
+            m_slider = GetComponent<Slider>();
+
+        m_slider.maxValue = p_health;
+        m_slider.value = p_health;
+
+        m_fill.color = m_gradient.Evaluate(1.0f);
+    }
+
+    public void SetHealth(int p_health)
+    {
+        m_slider.value = p_health;
+
+        m_fill.color = m_gradient.Evaluate(m_slider.normalizedValue);
+    }
+
+}
